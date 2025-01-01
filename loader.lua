b8a784d4b6f8fd5fda563d7520f58c665e727576
hacksCreator101 <denjabecka4@gmail.com>
2025-01-01 02:56:39 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+local loader = loadstring(game:Httpget("https://raw.githubusercontent.com/hacksCreator101/infyieldcustom/refs/heads/main/source.lua"))();
+
+addButton("ID", "Text", function()
+
+end)
